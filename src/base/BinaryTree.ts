2558cab5577e9259e905f88a6fc02ca4a6d45cd6
sucaizi <suzhaoda@outlook.com>
2019-09-02 17:34:51 +0800

    feat(binary tree): binary tree

---
+++ src/base/BinaryTree.ts
@@ -0,0 +1,43 @@
+// input [8,3,6,4,9,11,2,5,7];
+// output
+// let tree = {
+//     key: 8,
+//     left: {
+//         key: 3,
+//         left: {
+//             key: 2,
+//             left: null,
+//             right: null
+//         },
+//         right: {
+//             key: 6,
+//             left: {
+//                 key: 4,
+//                 left: null,
+//                 right: {
+//                     key: 5,
+//                     left: null,
+//                     right: null
+//                 }
+//             },
+//             right: {
+//                 key: 7,
+//                 left: null,
+//                 right: null
+//             }
+//         }
+//     }
+//     right: {
+//         key: 9,
+//         left: null,
+//         right: {
+//             key: 11,
+//             left: null,
+//             right: null
+//         }
+//     }
+// }
+
+export class BinaryTree {
+
+}
